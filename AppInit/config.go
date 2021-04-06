c4199cc4b206226ae51ff1a118f8040ea009a2d5
xyl <416319808@qq.com>
2021-04-06 13:44:37 +0800

    交叉编译

--- AppInit/config.go
+++ AppInit/config.go
@@ -1,11 +1,11 @@
 package AppInit
 
 const (
-	Address=":8080"
+	Address = ":8080"
 
-	driver="mysql"
-	dbUser="root"
-	dbPassword="root"
-	dbHost="127.0.0.1:3306"
-	database="test"
+	driver     = "mysql"
+	dbUser     = "root"
+	dbPassword = "123456"
+	dbHost     = "127.0.0.1:3306"
+	database   = "test"
 )
